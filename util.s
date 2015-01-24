c42a3ed4ac1501ac61e07f274ea30af9c67b3713
Julian Squires <julian@cipht.net>
2015-01-24 16:13:30 -0500

    Add trivial debug display routine

--- util.s
+++ util.s
@@ -28,4 +28,26 @@ wait_for_start_toggled:
 
         ldmfd sp!, {pc}
 
+        @@ r0 = word to print
+        .global print_debug_word
+print_debug_word:
+        mov r3, #vram_base	@ beginning
+        ldr r2, =hex_xlat
+        .macro m_print n=32
+          lsr r1, r0, #\n-4
+          and r1, r1, #0xf
+          ldrb r1, [r2, r1]
+          strh r1, [r3], #2
+          .if \n-4
+          m_print "(\n-4)"
+          .endif
+        .endm
+        m_print
+        mov pc,lr
+
+        .section .rodata
+hex_xlat:
+        .byte 32+26, 32+27, 32+28, 32+29, 32+30, 32+31, 32+32, 32+33, 32+34
+        .byte 32+35, 32, 32+1, 32+2, 32+3, 32+4, 32+5
+
 @ EOF util.s
